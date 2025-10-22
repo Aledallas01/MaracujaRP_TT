45c14dc650339c3883ec350d72c46a575ae7b05d
Aledallas01 <147440074+Aledallas01@users.noreply.github.com>
2025-10-22 19:08:44 +0200

    Risolto il bug per il quale l'upload richiedeva login con Discord

--- middleware.ts
+++ middleware.ts
@@ -2,14 +2,6 @@ export { default } from "next-auth/middleware";
 
 export const config = {
   matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api/auth (auth endpoints)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     * - login (login page)
-     */
-    "/((?!api/auth|_next/static|_next/image|favicon.ico|login).*)",
+    "/((?!api/auth|api/upload-transcript|_next/static|_next/image|favicon.ico|login).*)",
   ],
 };

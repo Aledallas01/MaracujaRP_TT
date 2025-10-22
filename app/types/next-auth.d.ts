062fa5b593d19f9ba756d2f597a4b3fff5b6695d
Aledallas01 <147440074+Aledallas01@users.noreply.github.com>
2025-10-22 17:33:51 +0200

    Fix

---
+++ app/types/next-auth.d.ts
@@ -0,0 +1,24 @@
+import { DefaultSession, DefaultUser } from "next-auth";
+import { JWT as DefaultJWT } from "next-auth/jwt";
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string;
+      discordId: string;
+    } & DefaultSession["user"];
+    accessToken?: string;
+  }
+
+  interface User extends DefaultUser {
+    id: string;
+    discordId: string;
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT extends DefaultJWT {
+    accessToken?: string;
+    discordId?: string;
+  }
+}
